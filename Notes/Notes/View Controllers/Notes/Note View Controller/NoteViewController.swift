07b8a2c325747838c83c000c04961b52a04f22bf
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-09-17 19:50:37 +0300

    Cap. 15 - Fix That Mistake

--- Notes/Notes/View Controllers/Notes/Note View Controller/NoteViewController.swift
+++ Notes/Notes/View Controllers/Notes/Note View Controller/NoteViewController.swift
@@ -15,16 +15,44 @@ class NoteViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     
+    // MARK: -
+    var note: Note?
+    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Edit Note"
+        
+        setupView()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        
+        // Update Note
+        if let title = titleTextField.text, !title.isEmpty {
+            note?.title = title
+        }
+        
+        note?.updatedAt = Date()
+        note?.contents = contentsTextView.text
+    }
+    
+    private func setupView() {
+        setupTitleTextField()
+        setupContentsTextView()
+    }
+    
+    private func setupTitleTextField() {
+        // Configure Title Text Field
+        titleTextField.text = note?.title
+    }
+    
+    private func setupContentsTextView() {
+        // Configure Contents Text View
+        contentsTextView.text = note?.contents
     }
    
     // MARK: - Navigation
