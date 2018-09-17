f5ae3f69aae1f3f160c093ad27f3b37e381c652e
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-09-17 14:13:34 +0300

    Cap. 14 - Fetch Those Notes User Interface Updated

--- Notes/Notes/View Controllers/Notes View Controller/NotesViewController.swift
+++ Notes/Notes/View Controllers/Notes View Controller/NotesViewController.swift
@@ -20,12 +20,34 @@ class NotesViewController: UIViewController {
     
     // MARK: - Properties
     
+    @IBOutlet weak var notesVIew: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: -
+    
     private let coreDataManager = CoreDataManager(modelName: "Notes")
+    
+    // MARK: -
+    
+    private let estimatedRowHeight = CGFloat(44.0)
+    
+    // MARK: -
+    
+    private lazy var updatedAtDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM d, HH:mm"
+        return dateFormatter
+    }()
     
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Notes"
+        
+        setupView()
     }
 
     // MARK: - Navigation
@@ -44,6 +66,56 @@ class NotesViewController: UIViewController {
         
     }
     
+    // MARK: - View Methods
+    
+    private func setupView() {
+        setupMessageLabel()
+        setupTableView()
+    }
+    
+    // MARK: -
+    
+    private func updateView() {
+        
+    }
+    
+    // MARK: -
+    
+    private func setupMessageLabel() {
+        messageLabel.text = "You don't have any notes yet."
+    }
+    
+    // MARK: -
+    
+    private func setupTableView() {
+        tableView.isHidden = true
+        tableView.estimatedRowHeight = estimatedRowHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
 
+}
+
+extension NotesViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Dequeue Reusable Cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NoteTableViewCell.reuseIdentifier, for: indexPath) as? NoteTableViewCell else {
+            fatalError("Unexpected Index Path")
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
 }
 
---
+++ Notes/Notes/View Controllers/Notes View Controller/Table View Cells/NoteTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  NoteTableViewCell.swift
+//  Notes
+//
+//  Created by Marian Stanciulica on 17/09/2018.
+//  Copyright © 2018 Marian Stanciulica. All rights reserved.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+
+    // MARK: - Static Properties
+    
+    static let reuseIdentifier = "NoteTableViewCell"
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentsLabel: UILabel!
+    @IBOutlet weak var updatedAtLabel: UILabel!
+    
+    // MARK: - Initialization
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
