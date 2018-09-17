657123abc8d911fff23314b314de9f0a4f957299
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-09-17 09:57:12 +0300

    Cap. 5 - Setting Up the Core Data Stack

---
+++ Notes/Notes/Managers/CoreDataManager.swift
@@ -0,0 +1,77 @@
+//
+//  CoreDataManager.swift
+//  Notes
+//
+//  Created by Marian Stanciulica on 17/09/2018.
+//  Copyright © 2018 Marian Stanciulica. All rights reserved.
+//
+
+import CoreData
+
+final class CoreDataManager {
+    
+    // MARK: - Properties
+    
+    private let modelName: String
+    
+    // MARK: - Initialization
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    private(set) lazy var managedObjectContext: NSManagedObjectContext = {
+        
+        // Initialize Managed Object Context
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        
+        // Configure Managed Object Context
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+        
+        return managedObjectContext
+    }()
+    
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        
+        // Fetch Model URL
+        guard let modelURL = Bundle.main.url(forResource: self.modelName, withExtension: "momd") else {
+            fatalError("Unable to Find Data Model")
+        }
+        
+        // Initialize Managed Object Model
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to Load Data Model")
+        }
+        
+        return managedObjectModel
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+
+        // Initialize Persistent Store Coordinator
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        
+        // Helpers
+        let fileManager = FileManager.default
+        let storeName = "\(self.modelName).sqlite"
+        
+        // URL Documents Directory
+        let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        // URL Persistent Store
+        let persistentStoreURL = documentsDirectoryURL.appendingPathComponent(storeName)
+
+        do {
+            // Add Persistent Store
+            let options = [NSMigratePersistentStoresAutomaticallyOption: true,
+                           NSInferMappingModelAutomaticallyOption: true]
+            
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: persistentStoreURL, options: options)
+        } catch {
+            fatalError("Unable to Add Persistent Store")
+        }
+        
+        return persistentStoreCoordinator
+    }()
+    
+}
